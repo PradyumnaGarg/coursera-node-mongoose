db01a6b20bcc5abb4126e953336c79871cbcbf35
Veteran <pradumnagarg@hotmail.com>
2020-05-08 01:04:52 +0530

    Mongoose part 2

--- index.js
+++ index.js
@@ -9,26 +9,24 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected to the server correctly");   
     
-    var newDish = Dishes({
+    Dishes.create({
         name : "Pizza",
         description: "Big"
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({});      
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});      
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
